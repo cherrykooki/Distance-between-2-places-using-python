c79050ea9f41f5b4476e6106bc31a86475fa130a
cherrykooki <97518721+cherrykooki@users.noreply.github.com>
2022-01-14 00:52:14 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#Calculate distance and duration between two places using google distance matrix API in Python
+# importing required libraries
+import requests, json
+
+# enter your api key here
+api_key ='Your_api_key'
+
+# Take source as input
+source = input()
+
+# Take destination as input
+dest = input()
+
+# url variable store url
+url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
+
+# Get method of requests module
+# return response object
+r = requests.get(url + 'origins = ' + source +
+				'&destinations = ' + dest +
+				'&key = ' + api_key)
+					
+# json method of response object
+# return json format result
+x = r.json()
+
+# by default driving mode considered
+
+# print the value of x
+print(x)
